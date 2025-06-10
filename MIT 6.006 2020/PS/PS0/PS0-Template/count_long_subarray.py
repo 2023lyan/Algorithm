eb136bcc185515f77c74eed20cdca21d76fb6e56
Yucheng Bai <2023lyan@sjtu.edu.cn>
2025-06-10 22:44:16 +0800

    feat: PS0 finished

--- MIT 6.006 2020/PS/PS0/PS0-Template/count_long_subarray.py
+++ MIT 6.006 2020/PS/PS0/PS0-Template/count_long_subarray.py
@@ -4,7 +4,23 @@ def count_long_subarray(A):
     Output: count | number of longest increasing subarrays of A
     '''
     count = 0
+    current_length = 1
+    max_length = 0
     ##################
-    # YOUR CODE HERE #
+    for i in range(1, len(A)):
+        if A[i] > A[i - 1]:
+            current_length += 1
+        else:
+            if current_length > max_length:
+                max_length = current_length
+                count = 1
+            elif current_length == max_length:
+                count += 1
+            current_length = 1
+    if current_length > max_length:
+        max_length = current_length
+        count = 1
+    elif current_length == max_length:
+        count += 1
     ##################
     return count
